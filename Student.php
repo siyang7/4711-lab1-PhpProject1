93ac4d51ff4989fb8d403ad1cd47ef89f8153545
Si-Yang Wu <siyang7@hotmail.com>
2018-01-10 12:07:18 -0800

    Lab 1 complete

--- Student.php
+++ Student.php
@@ -13,14 +13,17 @@ class Student {
 		$this->grades = array();
 	}
 
+	// function to add emails
 	function add_email($which, $address) {
 		$this->emails[$which] = $address;
 	}	
 
+	// function to add grades
 	function add_grade($grade) {
 		$this->grades[] = $grade;
 	}
 
+	// function that finds the average of the grades
 	function average() {
     		$total = 0;
     		foreach ($this->grades as $value)
@@ -28,6 +31,7 @@ class Student {
     		return $total / count($this->grades);
 	}
 
+	// function that converts the student's info into a string
 	function toString() {
     		$result = $this->first_name . ' ' . $this->surname;
     		$result .= ' ('.$this->average().")\n";
--- index.php
+++ index.php
@@ -1,5 +1,49 @@
 <?php
+include ('Student.php');
 
-echo "hello out there";
+$students = array();
+
+// Add first student
+$first = new Student();
+$first->surname = "Doe";
+$first->first_name = "John";
+$first->add_email('home','john@doe.com');
+$first->add_email('work','jdoe@mcdonalds.com');
+$first->add_grade(65);
+$first->add_grade(75);
+$first->add_grade(55);
+$students['j123'] = $first;
+
+// Add second student
+$second = new Student();
+$second->surname = "Einstein";
+$second->first_name = "Albert";
+$second->add_email('home','albert@braniacs.com');
+$second->add_email('work1','a_einstein@bcit.ca');
+$second->add_email('work2','albert@physics.mit.edu');
+$second->add_grade(95);
+$second->add_grade(80);
+$second->add_grade(50);
+$students['a456'] = $second;
+
+// Add 3rd student
+$third = new Student();
+$third->surname = "Dylan";
+$third->first_name = "Bob";
+$third->add_email('home', 'bob@home.com');
+$third->add_email('work1', 'bob@work1.com');
+$third->add_email('work2', 'bob@work2.com');
+
+$third->add_grade(90);
+$third->add_grade(75);
+$third->add_grade(65);
+$students['b777'] = $third;
+
+ksort($students); // sorts student names
+
+// outputs the students from Students class
+foreach($students as $student) {
+	echo $student->toString();
+}
 
 ?>
